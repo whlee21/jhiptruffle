7ff778465dbfc2c26eef7b26690531f99ffb4f23
whlee21 <whlee21@gmail.com>
2018-08-30 01:10:39 +0900

    adding crowdsale contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,14 @@
 //var FuckToken = artifacts.require("./FuckToken.sol");
 var SimpleToken = artifacts.require("./SimpleToken.sol");
 var SimpleCrowdsale = artifacts.require("./SimpleCrowdsale.sol");
+//var GustavoCrowdsale = artifacts.require("./GustavoCrowdsale.sol");
 
 module.exports = function(deployer) {
     const openingTime = web3.eth.getBlock('latest').timestamp + 2; // two secs in the future
     const closingTime = openingTime + 86400 * 20; // 20 days
     const rate = new web3.BigNumber(1000);
-    const wallet = '0x46752fc25d1BFE38c0966677B0a9190587b340b5';
+    //const wallet = '0x46752fc25d1BFE38c0966677B0a9190587b340b5';
+    const wallet = '0xB1da708E9c1848938105deb0e7ac0BE7f0c11469';
 
 
     return deployer
@@ -18,6 +20,7 @@ module.exports = function(deployer) {
         .then(() => {
             return deployer.deploy(
                 SimpleCrowdsale,
+                //GustavoCrowdsale,
                 openingTime,
                 closingTime,
                 rate,
